ef082652564fc9bc19a892931532d66bc02afc4a
wanghao22 <962165793@qq.com>
2019-11-12 10:25:20 +0800

    use

---
+++ DelTmpFiles.bat
@@ -0,0 +1,31 @@
+del /a /f /s /q *.ncb
+del /a /f /s /q *.ilk
+del /a /f /s /q *.pdg
+del /a /f /s /q *.user
+del /a /f /s /q *.manifest
+del /a /f /s /q *.dep
+del /a /f /s /q *.obj
+del /a /f /s /q *.idb
+del /a /f /s /q *.pdb
+del /a /f /s /q *.pch
+del /a /f /s /q *.res
+del /a /f /s /q *.exp
+del /a /f /s /q *.htm
+del /a /f /s /q *.sdf
+del /a /f /s /q *.ipch
+del /a /f /s /q *.iobj
+del /a /f /s /q *.ipdb
+del /a /f /s /q *.jpg
+del /a /f /s /q *.png
+del /a /f /s /q *.bmp
+del /a /f /s /q *.tiff
+del /a /f /s /q *.xls
+del /a /f /s /q *.tlog
+del /a /f /s /q *.lastbuildstate
+del /a /f /s /q *.cache
+del /a /f /s /q *.CppClean
+del /a /f /s /q *.enc
+del /a /f /s /q *.log
+del /a /f /s /q *.VC.db
+pause
+
